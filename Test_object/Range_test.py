cccea1f2c85e163fec82140568d8dd25960fdaaf
Silver Zh <di2vicx@gmail.com>
2021-10-08 22:27:36 +0700

    update

---
+++ Test_object/Range_test.py
@@ -0,0 +1,5 @@
+import random
+N = 10
+for i in range(N):
+    print(i + (1+ random.random() * 100))
+
--- Test_object/try_except.py
+++
@@ -1,9 +0,0 @@
-
-try:
-    value = 10/0
-    number = int(input("Enter a number: "))
-    print(number)
-except ZeroDivisionError as err_0:
-    print(err_0)
-except ValueError:
-    print("Invalid input")
