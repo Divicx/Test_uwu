07c66646104736aa7c76b7dcb12114495afc8251
Silver Zh <di2vicx@gmail.com>
2021-10-14 10:34:28 +0700

    cleaning

--- Test_object/main.py
+++
@@ -1,4 +0,0 @@
-
-n1, n2 = 5, 6
-table = [x*2 for x in range(n2)]
-print(table)
--- Test_object/simple_class.py
+++
@@ -1,6 +0,0 @@
-from student import Student
-
-student1 = Student("John", "Biology", 3.1, False)
-student2 = Student("Nelly", "Math", 4.1, True)
-
-print(student1.name)
